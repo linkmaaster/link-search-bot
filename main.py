b10dedba679dd65772ee17cd7d0cc5643519924d
JayR1213 <111758139+JayR1213@users.noreply.github.com>
2023-02-25 21:01:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -114,6 +114,7 @@ async def message_handler(event):
                                     f'http://www.google.com/search?q={event.text.replace(" ", "%20")}%20Movie')], [
                             Button.url('Click To Check Release Date 📅',
                                     f'http://www.google.com/search?q={event.text.replace(" ", "%20")}%20Movie%20Release%20Date')],
+
             await txt.delete()
             result = await event.reply(answer, buttons=newbutton, link_preview=False)
             await asyncio.sleep(Config.AUTO_DELETE_TIME)
